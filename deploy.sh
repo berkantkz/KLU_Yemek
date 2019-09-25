9ed10b70550f70e2d13953347a3a27387804c0b4
Berkant Korkmaz <berkantk3@gmail.com>
2019-09-26 00:25:46 +0300

    workflow: export token

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 git config --global user.email 'berkantk3@gmail.com'
 git config --global user.name 'Berkant Korkmaz'
-git remote set-url origin https://berkantkz:${{ secrets.token }}@github.com/berkantkz/KLU_Yemek.git
+git remote set-url origin https://berkantkz:$token@github.com/berkantkz/KLU_Yemek.git
 if git status --porcelain | grep -q 'list.json'; then 
 	git add list.json
 	git commit -am 'update list'
-	echo berkantkz | echo ${{ secrets.token }} | git push origin HEAD:master	
+	echo berkantkz | echo $token | git push origin HEAD:master	
 else
 	echo 'No changes detected'
 fi
